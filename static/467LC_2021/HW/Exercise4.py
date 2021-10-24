327f89b1e62084b402f9fc11b4488ceb916552e7
Sakol <Sakol@Sakol.local>
2021-10-24 21:09:13 +0700

    LG467 update

---
+++ static/467LC_2021/HW/Exercise4.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 24 20:55:41 2021
+
+@author: Sakol
+"""
+
+# Instructions: Tag the four sentences below using SpaCy. 
+# Then, for each sentence, 
+# (a) print out word indices, tokens, and tags; and
+# (b) count POS tags and print out those tags and their frequency counts
+
+# Note: A few words are mislabled by the POS tagger. You may ignore this.
+
+
+
+# Save the script using the following convention: yourname_pos.py
+# Submit the script through Microsoft Teams.
+# This exercise is worth 10 points.
+
+
+
+
+txt1 = "All the faith he had had had had no effect on the outcome of this life."
+txt2 = "The complex houses married and single soldiers and their families."
+txt3 = "Across the developing world, hundreds of millions of people are unable to get a vaccine to protect themselves from the ravages of Covid-19."
+txt4 = 'This week, the EPA announced a new roadmap to research, restrict, and remediate PFAS, a group of industrial "forever chemicals" that have been linked to cancer and are found in our food, water, and even our blood.'
